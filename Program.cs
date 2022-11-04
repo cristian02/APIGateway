16939fdfd95324547c7c77ff30dc801807afbc55
Cristian Martinez <crismh2@hotmail.com>
2022-11-04 11:27:22 -0600

    Change Postgres to SQLServer

--- Program.cs
+++ Program.cs
@@ -7,9 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<AppDbContext>(opt=>opt.UseNpgsql(builder.Configuration.GetConnectionString("SampleDbConnection")));
+builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseSqlServer(builder.Configuration.GetConnectionString("SampleDbConnection")));
+
+//builder.Services.AddEntityFrameworkNpgsql().AddDbContext<AppDbContext>(opt=>opt.UseNpgsql(builder.Configuration.GetConnectionString("SampleDbConnection")));
 
 var app = builder.Build();
 
